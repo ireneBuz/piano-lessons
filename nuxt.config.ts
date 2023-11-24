23ceb7031d8388541415c1b0797d0880394a8633
Irene Buceta <irenebuzalocen@gmail.com>
2023-11-24 01:12:13 +0100

    gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,7 +36,18 @@ export default defineNuxtConfig({
         }
       ],
       "style": [],
-      "script": [{ src: 'https://code.iconify.design/1/1.0.4/iconify.min.js', defer: true }],
+      "script": [{ src: 'https://code.iconify.design/1/1.0.4/iconify.min.js', defer: true },
+      {
+        innerHTML: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){ dataLayer.push(arguments); }
+          gtag('js', new Date());
+          gtag('config', 'G-XRT72ZGBB4');
+        `,
+        type: 'text/javascript',
+        defer: true
+      },
+      { src: 'https://www.googletagmanager.com/gtag/js?id=G-XRT72ZGBB4', async: true }],
       "noscript": [],
       "title": "Music lessons - Irene Buceta",
       htmlAttrs: {
